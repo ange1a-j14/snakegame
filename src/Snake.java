67d55003509dde72c8ee8c0f1fdd06d449520715
Angela Jia <42385351+22angiej@users.noreply.github.com>
2019-10-11 15:03:04 -0700

    Continue to debug

--- src/Snake.java
+++ src/Snake.java
@@ -13,13 +13,16 @@ public class Snake {
    public enum Direction {
       LEFT(0, -1), RIGHT(0, 1), UP(-1, 0), DOWN(1, 0);
       private int rPlus, cPlus;
+
       private Direction(int rPlus, int cPlus) {
          this.rPlus = rPlus;
          this.cPlus = cPlus;
       }
+
       public int returnRPlus() {
          return rPlus;
       }
+
       public int returnCPlus() {
          return cPlus;
       }
@@ -39,68 +42,65 @@ public class Snake {
    public boolean oneSuccessfulStep() {
       boolean hit = false;
       updateFood = false;
+      eat();
       hit = !moveJoints();
-      if(hit)
+      if (hit)
          return false;
       eat();
       return true;
    }
 
-   public boolean moveJoints() { 
+   public boolean moveJoints() {
       directionCalibration();
       Location currHeadLoc = snakeJoints.get(0).getLocation();
       Location aboveJointLoc = currHeadLoc;
-      Location newHeadLoc = new Location(currHeadLoc.getRow() + 
-                                       currDirection.returnRPlus(),
-                                       currHeadLoc.getCol() + 
-                                       currDirection.returnCPlus());
-      if(canHeadMoveTo(newHeadLoc)) {
+      Location newHeadLoc = new Location(currHeadLoc.getRow() + currDirection.returnRPlus(),
+            currHeadLoc.getCol() + currDirection.returnCPlus());
+      if (canHeadMoveTo(newHeadLoc)) {
          snakeJoints.get(0).moveTo(newHeadLoc);
       } else {
          return false;
       }
-      for(int i = 1; i < snakeJoints.size(); i++) {
+      for (int i = 1; i < snakeJoints.size(); i++) {
          Block currJoint = snakeJoints.get(i);
          Location temp = currJoint.getLocation();
-         System.out.println(snakeJoints);
          currJoint.moveTo(aboveJointLoc);
          aboveJointLoc = temp;
-         }
+      }
       return true;
    }
 
    private void directionCalibration() {
-      if(currDirection == Direction.LEFT && prevDirection == Direction.RIGHT ||
-         currDirection == Direction.RIGHT && prevDirection == Direction.LEFT ||
-         currDirection == Direction.UP && prevDirection == Direction.DOWN ||
-         currDirection == Direction.DOWN && prevDirection == Direction.UP)
+      if (currDirection == Direction.LEFT && prevDirection == Direction.RIGHT
+            || currDirection == Direction.RIGHT && prevDirection == Direction.LEFT
+            || currDirection == Direction.UP && prevDirection == Direction.DOWN
+            || currDirection == Direction.DOWN && prevDirection == Direction.UP)
          currDirection = prevDirection;
    }
 
    private boolean canHeadMoveTo(Location loc) {
-      if(grid.isValid(loc)) {
+      if (grid.isValid(loc)) {
          Block b = (Block) grid.get(loc);
-         if(b == null)
+         if (b == null)
             return true;
-         if(b.getGridElement() == Block.GridElement.FOOD) {
+         if (b.getGridElement() == Block.GridElement.FOOD) {
             onFood = true;
-         } 
-         return b.getGridElement() != Block.GridElement.SNAKEJOINT;
+            return true;
+         }
       }
       return false;
    }
 
    private void eat() {
-      if(onFood) {
+      if (onFood) {
          updateFood = true;
          System.out.println("eat");
          Block b = new Block(Block.GridElement.SNAKEJOINT);
-         Block lastJoint = snakeJoints.get(snakeJoints.size()-1);
-         Location bLoc = new Location(lastJoint.getLocation().getRow()- 
-                                       currDirection.returnRPlus(),
-                                       lastJoint.getLocation().getCol()-
-                                       currDirection.returnCPlus());
-         b.putSelfInGrid(grid, bLoc);
+         Block lastJoint = snakeJoints.get(snakeJoints.size() - 1);
+         Location bLoc = new Location(lastJoint.getLocation().getRow() - currDirection.returnRPlus(),
+               lastJoint.getLocation().getCol() - currDirection.returnCPlus());
+         if(grid.isValid(bLoc))
+            b.putSelfInGrid(grid, bLoc);
          snakeJoints.add(b);
          onFood = false;
       }
